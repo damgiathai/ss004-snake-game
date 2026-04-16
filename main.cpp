9ff6c6184a88965be7caa100587c2c9ba55001ab
Tinily0310 <9a1diem@gmail.com>
2026-04-16 21:58:47 +0700

    ve-khung

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <time.h>
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 using namespace std;
 void gotoxy( int column, int line );
 struct Point{
@@ -30,10 +35,24 @@ public:
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
+        if (A[0].x == MINX || A[0].x == MAXX ||
+    A[0].y == MINY || A[0].y == MAXY) {
+    cout << "GAME OVER";
+    exit(0);
+}
 
     }
 };
-
+void VeKhung() {
+    for (int i = MINX; i <= MAXX; i++) {
+        for (int j = MINY; j <= MAXY; j++) {
+            if (i == MINX || i == MAXX || j == MINY || j == MAXY) {
+                gotoxy(i, j);
+                printf("#");
+            }
+        }
+    }
+}
 int main()
 {
     CONRAN r;
@@ -49,6 +68,7 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
+        VeKhung();
         r.Ve();
         r.DiChuyen(Huong);
         Sleep(300);
